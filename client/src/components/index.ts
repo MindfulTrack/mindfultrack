011248d083dab4fa539b54ff3cb48f7d0aba5b55
Jacob D <jwdonaldson99@gmail.com>
2024-01-10 08:37:36 -0700

    next js front end fixed

--- client/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button } from "./button";
-export { Cards } from "./cards";
-export { Footer } from "./footer";
-export { Header } from "./header";
-export { Logo } from "./logo";
-export { Main } from "./main";
