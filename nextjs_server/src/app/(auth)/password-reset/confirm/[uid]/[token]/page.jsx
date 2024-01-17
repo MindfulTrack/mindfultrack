cf15d07be47220906e8a57bb2bdca1a615fcc68a
Jacob D <jwdonaldson99@gmail.com>
2024-01-17 11:20:19 -0700

    component routes fix

--- nextjs_server/src/app/(auth)/password-reset/confirm/[uid]/[token]/page.jsx
+++ nextjs_server/src/app/(auth)/password-reset/confirm/[uid]/[token]/page.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import PasswordResetForm from "./form";
-import Copyright from '@/components/Copyright'
+import Copyright from '@/app/components/Copyright'
 
 // TODO remove, this demo shouldn't need to reset the theme.
 export default function PasswordReset({params}) {
