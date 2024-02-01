ebe020522f05c4f774489ce755445463584b991a
SpencerKing7 <spencerking.c@gmail.com>
2024-01-31 22:59:56 -0700

    student availability layout draft

--- nextjs_server/src/app/(loggedin)/(student)/availability/page.tsx
+++ nextjs_server/src/app/(loggedin)/(student)/availability/page.tsx
@@ -1,22 +1,49 @@
+'use client'
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 import ResourceCard from '../../../components/ResourceCard';
-import { Paper } from '@mui/material';
+import { Container, Paper } from '@mui/material';
+import timeSlots from '../../../mockData/timeSlots.json';
+import dayOfWeek from '../../../mockData/dayOfWeek.json';
+import { useState } from 'react';
 
 interface StudentAvailabilityPageProps {
 
 };
 
+
 const StudentAvailabilityPage: React.FC<StudentAvailabilityPageProps> = () => {
+  const [selectedTime, setSelectedTime] = useState(false);
+  
+  const handleTimeSelect = (id: number) => {
+    setSelectedTime(!selectedTime)
+  }
+
+  const dayTimeSlots = timeSlots;
+  const weekDays = dayOfWeek;
   return (
     <Box>
-      <div>
-        <Paper sx={{backgroundColor: "secondary.main", height: "4rem", marginBottom: 2}}>
-            <Typography variant='h4' color="text.tertiary" sx={{textAlign: "left", paddingLeft: "15px", verticalAlign: 'middle'}}>AVAILABILITY</Typography>
+      <Box>
+        <Paper sx={{ backgroundColor: "#c5c5c5", height: "2rem", marginBottom: 2 }}>
+          <Typography variant='body1' color="text.main" sx={{ textAlign: "center" }}>Select the times that you would be available for an appointment during <strong>this semester.</strong></Typography>
         </Paper>
-      </div>
+      </Box>
+
+      <Box component={'div'} style={{display: 'flex', justifyContent: 'space-evenly', paddingTop: '2rem'}}>
+        {weekDays.map((day) => (
+          <Box key={day.dayOfWeek} style={{alignContent: 'center'}}>
+            <Typography variant='h2' textAlign={'center'} color={'primary'} sx={{paddingBottom: '1rem'}}>{day.dayOfWeek}</Typography>
+
+            {dayTimeSlots.map((time) => (
+              <Box key={time.startTime} sx={{border: 'solid', borderWidth: '1px', borderRadius: '4px', margin: '3px', width: '150px', cursor: 'pointer', backgroundColor: selectedTime ? '#e0ffe5' : ''}} onClick={() => handleTimeSelect(time.timeSlotID)}>
+                <Typography textAlign={'center'}>{time.startTime}</Typography>
+              </Box>
+            ))}
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 };
