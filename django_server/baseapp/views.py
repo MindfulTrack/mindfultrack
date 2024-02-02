0a051691eb6e9ca7f66300040db9970fa9577153
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2024-02-02 07:14:11 -0700

    Django serializer fix

--- django_server/baseapp/views.py
+++ django_server/baseapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Test, StudentQueue
-from .serializers import TestSerializer, StudentQueueSerializer
+from .serializers import TestSerializer
 from django.shortcuts import get_object_or_404
 from datetime import datetime
 
