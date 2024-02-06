e4010e514117d00b3e644d5117a69e8e75cfda1c
Jacob D <jwdonaldson99@gmail.com>
2024-02-06 08:56:18 -0700

    healthcheck on frontend

--- nextjs_server/src/app/layout.tsx
+++ nextjs_server/src/app/layout.tsx
@@ -9,35 +9,44 @@ import theme from '../theme';
 import NavBar from './components/NavBar';
 import MyProvider from './MyProvider';
 import { authOptions } from './api/auth/[...nextauth]/authOptions.js';
+import { headers } from 'next/headers';
 
 interface RootLayoutProps {
   children: ReactNode
 }
 
 const RootLayout: React.FC<RootLayoutProps> = async ({children}) => {
-  const op : any = authOptions;
-  const session : any = await getServerSession();
-  
-  return (
-    <html lang="en">
-      <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <SessionProvider session={session}>
-            <ThemeProvider theme={theme}>
-              <MyProvider>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
+  const headersList = headers();
+  const currentUrl = headersList.get('next-url')
+ 
+  if(currentUrl != '/test'){
+    const op : any = authOptions;
+    const session : any = await getServerSession();
+    
+    return (
+      <html lang="en">
+        <body>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <SessionProvider session={session}>
+              <ThemeProvider theme={theme}>
+                <MyProvider>
+                  {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                  <CssBaseline />
 
-                <NavBar />
+                  <NavBar />
 
-                {children}
-              </MyProvider>
-            </ThemeProvider>
-          </SessionProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
-  );
+                  {children}
+                </MyProvider>
+              </ThemeProvider>
+            </SessionProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    );
+  }
+  else{
+    return
+  }
 }
 
 export default RootLayout;
