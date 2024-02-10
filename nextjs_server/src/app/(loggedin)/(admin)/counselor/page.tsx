7217714e9dbbd93ba8670b0738103cf13fa4bd3a
Jacob D <jwdonaldson99@gmail.com>
2024-02-10 10:00:37 -0700

    api call

--- nextjs_server/src/app/(loggedin)/(admin)/counselor/page.tsx
+++ nextjs_server/src/app/(loggedin)/(admin)/counselor/page.tsx
@@ -2,21 +2,32 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Paper } from '@mui/material';
+import customFetch from '../../../api/fetchInterceptor';
 
 interface CounselorPageProps {
 
 };
 
-const CounselorPage: React.FC<CounselorPageProps> = () => {
-  return (
-    <Box>
-        <Paper sx={{ backgroundColor: "#e6e6e6", padding: 2, marginTop: 2, marginBottom: 2, flex: '100%' }}>
-          <Typography variant='h2' color='text.main' sx={{ textAlign: 'left' }}>
-            Counselor Page
-          </Typography>
-        </Paper>
-      </Box>
-  );
+const CounselorPage: React.FC<CounselorPageProps> = async () => {
+
+  const data : any = await customFetch('base/studentQueue/4')
+  console.log(data.json())
+  const student = await Promise.all([data,])
+  if (!data) {
+   
+    return ("No Data Found")
+  }
+  else{
+    return (
+      <Box>
+          <Paper sx={{ backgroundColor: "#e6e6e6", padding: 2, marginTop: 2, marginBottom: 2, flex: '100%' }}>
+            <Typography variant='h2' color='text.main' sx={{ textAlign: 'left' }}>
+              Counselor Page {student.id}
+            </Typography>
+          </Paper>
+        </Box>
+    );
+  }
 };
 
 export default CounselorPage;
