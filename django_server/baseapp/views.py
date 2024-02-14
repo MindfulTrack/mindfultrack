467317f09311207de41e877df49b074295281a6f
Jacob D <jwdonaldson99@gmail.com>
2024-02-14 13:18:45 -0700

    api documentation through swagger

--- django_server/baseapp/views.py
+++ django_server/baseapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Test, StudentQueue, DayOfWeek, ResourceCategory, Resource
-from .serializers import TestSerializer, TestAuthSerializer, StudentQueueSerializer
+from .models import Test, StudentQueue, DayOfWeek, ResourceCategory, Resource, University
+from .serializers import TestSerializer, TestAuthSerializer, StudentQueueSerializer, UniversitySerializer
 from django.shortcuts import get_object_or_404
 from datetime import datetime
 from django.http import HttpResponse
@@ -108,3 +108,25 @@ class StudentQueueView(APIView):
     
 
 #University
+@permission_classes([IsAuthenticated])
+class UniversityView(APIView):
+    # GET all universities
+    def get (self, request, format=None):
+        university = University.objects.all()
+        serializer = UniversitySerializer(university, many=True)
+        return Response(serializer.data)
+
+    # POST a new university
+    def post (self, request):
+        serializer = UniversitySerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    # DELETE university
+    def delete (self, request, university_id):
+        university = get_object_or_404(University, id = university_id)
+        university.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
