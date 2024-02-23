a5dba4273b1066f82600272690af1117ca8369cd
Jacob D <jwdonaldson99@gmail.com>
2024-02-23 11:21:12 -0700

    fix to fetch interceptor

--- nextjs_server/src/app/api/fetchInterceptor.tsx
+++ nextjs_server/src/app/api/fetchInterceptor.tsx
@@ -2,22 +2,24 @@ import { authOptions } from './auth/[...nextauth]/authOptions.js';
 import { getServerSession } from "next-auth";
 
 
-async function customFetch(url : any, methodType : any = 'GET', data : any = {}, customOption : any = null) {
+async function customFetch(url : any, methodType : any = 'GET', data : any = null, customOption : any = null) {
   const op : any = authOptions;
   const session : any = await getServerSession(op);
   const token = session?.access_token;
   const baseUrl = process.env.NEXTAUTH_BACKEND_URL
 
   // if custom options specified then use custome options
-  const customOptions = customOption ? customOption : {
+  let customOptions = customOption ? customOption : {
     method: methodType, // *GET, POST, PUT, DELETE, etc.
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-    body:JSON.stringify(data),
   };
 
+  if(data){
+    customOptions['body'] =JSON.stringify(data)
+  }
   return fetch(baseUrl + url, customOptions);
 }
 
