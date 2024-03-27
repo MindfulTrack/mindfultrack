a5c4a4628fd27c05cba6215b446910cbe4ddf3ef
SpencerKing7 <spencerking.c@gmail.com>
2024-03-26 18:34:58 -0600

    spacing

--- nextjs_server/src/app/components/homeheading/homeheading.tsx
+++ nextjs_server/src/app/components/homeheading/homeheading.tsx
@@ -32,7 +32,7 @@ const HomeHeading: React.FC<HomeHeadingProps> = () => {
 
           <Grid container spacing={2} sx={{pt: 4}}>
             <Grid item lg={5}>
-              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '100%' }}>
+              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '100%', paddingTop: '2rem' }}>
                 <Link href="/signup"><Button variant="contained" sx={{ width: '80%' }} color="secondary">Sign Up</Button></Link>
                 <Typography variant="body1" sx={{ paddingRight: '5rem', pt: '2rem' }}>
                   Welcome to MindfulTrack, where we streamline your journey to mental wellness. 
@@ -46,7 +46,7 @@ const HomeHeading: React.FC<HomeHeadingProps> = () => {
               </div>
             </Grid>
             <Grid item lg={7}>
-              <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+              <div style={{ position: 'relative', width: '100%', height: '100%', paddingTop: '2rem' }}>
                 <Image
                   src={"https://mindfultrack-files.s3.us-east-2.amazonaws.com/images/BYUCampus.jpg"}
                   style={{ borderRadius: '15px' }}
